27f40cbca8d52bd44a7a544addd4b1225315a83d
Christopher Boyd <boyd.christopher.t@gmail.com>
2022-04-14 20:24:33 -0500

    Created boilerplate

---
+++ server/server.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+app.use(express.static('./server/public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
