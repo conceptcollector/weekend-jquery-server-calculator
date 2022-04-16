192a58a398d081042e7753217cd7228edc666883
Christopher Boyd <boyd.christopher.t@gmail.com>
2022-04-16 16:05:14 -0500

    calculations are working

--- server/public/client.js
+++ server/public/client.js
@@ -1,23 +1,47 @@
+let operator = '';
+
 $(document).ready(onReady);
 
 function onReady() {
+    $('.operator').on('click', operate);
     $('#equals').on('click', doMath);
 }
 
-function doMath(numberOne, numberTwo) {
+function operate() {
+    operator = $(this).text();
+}
+
+function doMath() {
     let mathEquation = {
-        firstNumber: numberOne,
-        math: $('.operator').val(),
-        secondNumber: numberTwo
+        firstNumber: $('#firstNumber').val(),
+        operator: operator,
+        secondNumber: $('#secondNumber').val()
     }
-    
     $.ajax({
         method: 'POST',
-        url: '/',
+        url: '/unifiedMath',
         data: mathEquation
     }).then(function (response){
         console.log(response);
     })
+    // Becase "readMath". Get it? 😅
     languageOfTheUniverse();
 }
 
+function languageOfTheUniverse(){
+    $.ajax({
+      method: 'GET',
+      url: '/unifiedMath'
+    }).then(function(response){
+      console.log('the server sent me something');
+      console.log(response);
+      $('#screen').empty();
+      $('#equations').empty();
+      $('#screen').append(`${response[response.length-1].solution}`);
+      for (let thisMath of response) {
+        $('#equations').append(`<li>
+        ${thisMath.firstNumber} ${thisMath.operator} ${thisMath.secondNumber} = ${thisMath.solution}
+        <li>`);
+      }
+    })
+  }
--- server/server.js
+++ server/server.js
@@ -4,5 +4,37 @@ const app = express();
 app.use(express.static('./server/public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+const PORT = 5000;
 
 let aBriefHistoryOfMath = [];
+let serverMath;
+
+app.post('/unifiedMath', (req, res) => {
+    console.log('POST/unifiedMath');
+    console.log(req.body);
+    serverMath = req.body;
+    actualMath();
+    res.sendStatus(200);
+  })
+
+function actualMath() {
+    if (serverMath.operator === '+') {
+        serverMath.solution = Number(serverMath.firstNumber) + Number(serverMath.secondNumber);
+    } else if (serverMath.operator === '-') {
+        serverMath.solution = Number(serverMath.firstNumber) - Number(serverMath.secondNumber);
+    } else if (serverMath.operator === '*') {
+        serverMath.solution = Number(serverMath.firstNumber) * Number(serverMath.secondNumber);
+    } else if (serverMath.operator === '/') {
+        serverMath.solution = Number(serverMath.firstNumber) / Number(serverMath.secondNumber);
+    }
+    aBriefHistoryOfMath.push(serverMath);
+}
+  
+  app.get('/unifiedMath', (req, res) => {
+    console.log('GET /unifiedMath');
+    res.send(aBriefHistoryOfMath);
+  })
+  
+  app.listen(PORT, () => {
+    console.log (`Server is running on at http://localhost:${PORT}`);
+  })
